9aa22c3bdee9074367b1187d5226ea39bc3c964a
patarabulki1234 <iliazabaxidze28@gmail.com>
2026-05-17 17:07:29 +0400

    Add files via upload

--- forms.py
+++ forms.py
@@ -1,11 +1,12 @@
 from flask_wtf import FlaskForm
 from wtforms.fields import StringField, PasswordField, IntegerField, DateField, SelectField, EmailField
-from wtforms.validators import DataRequired, length
+from wtforms.validators import DataRequired, length, equal_to
 
 
 class registerForm(FlaskForm):
     username = StringField("Enter Username", validators=[DataRequired()])
     password = PasswordField("Enter Password", validators=[DataRequired(), length(min=8, max=12)])
+    confirm_password = PasswordField("Confirm Password", validators=[DataRequired(), equal_to("password", message="Passwords must match")])
     email = EmailField("Enter Email", validators=[DataRequired()])
     mobile = IntegerField("Enter Mobile Number", validators=[DataRequired()])
     birthdate = DateField("Enter Birth Date", validators=[DataRequired()])
